dc9757a7d17445fc27513ff4702204f84d225b27
ethanwait25 <ethanwait25@gmail.com>
2024-03-21 23:01:30 -0600

    Added Register JavaScript

--- startup/login.js
+++ startup/login.js
@@ -3,3 +3,9 @@ function login() {
   localStorage.setItem("userName", nameEl.value);
   window.location.href = "index.html";
 }
+
+function register() {
+  const nameEl = document.querySelector("#usernameRegister");
+  localStorage.setItem("userName", nameEl.value);
+  window.location.href = "create.html";
+}
