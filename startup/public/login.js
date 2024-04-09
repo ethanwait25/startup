b19a1a1d5297205803acb84feb4c0dc7029fdf6a
ethanwait25 <ethanwait25@gmail.com>
2024-04-09 11:41:03 -0600

    Registration now fully populates the localStorage

--- startup/public/login.js
+++ startup/public/login.js
@@ -1,11 +1,49 @@
 function login() {
+  localStorage.clear();
   const nameEl = document.querySelector("#username");
   localStorage.setItem("userName", nameEl.value);
   window.location.href = "index.html";
 }
 
 function register() {
+  localStorage.clear();
   const nameEl = document.querySelector("#usernameRegister");
   localStorage.setItem("userName", nameEl.value);
+  const emailEl = document.querySelector("#email");
+  localStorage.setItem("email", emailEl.value);
   window.location.href = "create.html";
+
+  updateUserByte(25, localStorage.getItem("userName"));
+
+}
+
+async function updateUserByte(newScore, playerName) {
+  var requestBody = {
+      "playerName": playerName,
+      "score": newScore
+  }
+
+  const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(requestBody)
+  };
+
+  console.log(options);
+    
+  try {
+      const response = await fetch('/api/score', options);
+      scores = await response.json();
+
+      if (scores.status != 200) {
+          throw "Error updating user byte.";
+      }
+  
+      localStorage.setItem('userByte', newScore);
+    } catch {
+      console.log("Error updating user byte.")
+    }
 }
