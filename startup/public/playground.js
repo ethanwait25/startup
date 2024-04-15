dc6fc22482703275aecf85bc9d34bb8731af0ec2
ethanwait25 <ethanwait25@gmail.com>
2024-04-15 14:29:39 -0600

    Playground now displays active players

--- startup/public/playground.js
+++ startup/public/playground.js
@@ -1,42 +1,37 @@
-const challengers = [];
+var challengers = [];
 
-async function getChallengers() {
-const response = await fetch("/api/active", {
-    method: 'get',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
+(async () => {
+    const response = await fetch("/api/active", {
+        method: 'get',
+        headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
 
-  if (response.ok) {
-    challengers = await response.json();
-  } else {
-    console.log("Error getting challengers");
-  }
-}
+    if (response.ok) {
+        challengers = await response.json();
+    } else {
+        console.log("Error getting challengers");
+    }
 
-await getChallengers();
+    const container = document.querySelector("#chalsContainer");
 
-const container = document.querySelector("#chalsContainer");
+    challengers.forEach((item, index) => {
+        const div = document.createElement("div");
+        div.classList.add("chal");
+        div.id = `c${index + 1}`;
 
-console.log(container);
+        div.innerHTML = `
+        <div class="chalAvatar">
+            <a href="battle.html"><img alt="Challenger!" src=${item.image} /></a>
+        </div>
+        <div class="chalStats">
+            <p class="chalName">${item.prompt}</p>
+            <p class="chalByte">${item.byte} Byte</p>
+        </div>
+        `;
 
-challengers.forEach((item, index) => {
-    const div = document.createElement("div");
-    div.classList.add("chal");
-    div.id = `c${index + 1}`;
+        container.appendChild(div);
+    });
 
-    div.innerHTML = `
-      <div class="chalAvatar">
-        <a href="battle.html"><img alt="Challenger!" src=${item.image} /></a>
-      </div>
-      <div class="chalStats">
-        <p class="chalName">${item.name}</p>
-        <p class="chalByte">${item.byte}</p>
-      </div>
-    `;
-
-    console.log(div);
-
-    container.appendChild(div);
-  });
+})();
