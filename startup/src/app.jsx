1a27b1777faccc816cca65fe495310aaec4bec87
ethanwait25 <ethanwait25@gmail.com>
2024-04-17 14:55:13 -0600

    Created structure for work with view components

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import './home.css';
+import './home/home.css';
 
 export default function App() {
   return (
